65773d928e64dcc546a5113d142c05e9af66e1ce
michael <63001812@qq.com>
2018-04-08 17:58:01 +1200

    update04-08

--- src/components/business/business.js
+++ src/components/business/business.js
@@ -8,11 +8,11 @@ export class Business extends React.Component {
       return (
         <div className="Business">
           <div className="image-container">
-            <a href={this.props.business.url}><img src={this.props.business.imageUrl} alt="image" /></a>
+            <a target="_blank" href={this.props.business.url}><img src={this.props.business.imageUrl} alt="image" /></a>
           </div>
           <h2>{this.props.business.name}</h2>
           <div className="Business-information">
-            <div className="Business-address"><a  href={`https://maps.google.com/?q=${this.props.business.address}`} target="_blank">
+            <div className="Business-address"><a  target="_blank" href={`https://maps.google.com/?q=${this.props.business.address}`} target="_blank">
               <p>{this.props.business.address}</p>
               <p>{this.props.business.city}</p>
               <p>{this.props.business.state + " " + this.props.business.zipCode}</p>
--- src/components/initialrenderinglist.js
+++ src/components/initialrenderinglist.js
@@ -1,5 +1,7 @@
 
 
+
+
 export const initialList=
 [{
     address:"Pine Ave",
